e018f8c4864ba138372274a3a4ada8a1996b40cc
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-12-19 12:15:24 +0530

    Adding notify-slack module
    Supports posting Basic slack message via Webhooks

---
+++ notify-slack/src/main/java/in/ashwanthkumar/notify/BasicSlackNotifier.java
@@ -0,0 +1,36 @@
+package in.ashwanthkumar.notify;
+
+import in.ashwanthkumar.slack.webhook.Slack;
+import in.ashwanthkumar.slack.webhook.SlackMessage;
+
+import java.io.IOException;
+
+/**
+ * Basic Slack Notifier that helps in notification to Slack
+ */
+public class BasicSlackNotifier implements Notifier {
+
+    private Slack client;
+
+    public BasicSlackNotifier(Slack client) {
+        this.client = client;
+    }
+
+    @Override
+    public void error(String message) {
+        try {
+            client.push(new SlackMessage(message));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void info(String message) {
+        try {
+            client.push(new SlackMessage(message));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
