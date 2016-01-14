872b0f6cfeb287b34e015c6ab6cd75d9de5dda59
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-01-14 10:44:10 +0530

    always doing a clean before deploy

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@
 RELEASE_VERSION=`cat VERSION`
 echo "Publishing $RELEASE_VERSION to Maven Central"
 mvn versions:set --batch-mode --errors -DgenerateBackupPoms=false -DnewVersion="$RELEASE_VERSION"
-mvn deploy -DperformRelease -DskipTests --errors
+mvn clean deploy -DperformRelease -DskipTests --errors
