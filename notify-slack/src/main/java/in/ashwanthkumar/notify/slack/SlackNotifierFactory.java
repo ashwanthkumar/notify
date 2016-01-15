3d976c85c4df8a9de47f1ae13b41bb10e92baf01
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-01-15 11:57:13 +0530

    Adding support for more fields on Slack client via config

--- notify-slack/src/main/java/in/ashwanthkumar/notify/slack/SlackNotifierFactory.java
+++ notify-slack/src/main/java/in/ashwanthkumar/notify/slack/SlackNotifierFactory.java
@@ -12,7 +12,24 @@ public class SlackNotifierFactory {
         String type = config.getString("type");
         assert type.equalsIgnoreCase(NOTIFIER_NAME);
         String webHookUrl = config.getString("webhook-url");
+        Slack client = new Slack(webHookUrl);
 
-        return new BasicSlackNotifier(new Slack(webHookUrl));
+        if(config.hasPath("channel")) {
+            client.sendToChannel(config.getString("channel"));
+        }
+
+        if(config.hasPath("user")) {
+            client.sendToUser(config.getString("user"));
+        }
+
+        if(config.hasPath("botname")) {
+            client.displayName(config.getString("botname"));
+        }
+
+        if(config.hasPath("icon")) {
+            client.icon(config.getString("icon"));
+        }
+
+        return new BasicSlackNotifier(client);
     }
 }
